0a478e003f20dcc97c5d8e5a4cc48a4c20b34812
Liu Chang <lc3018121@sjtu.edu.cn>
2017-06-22 11:35:13 +0800

    Sort according to history

--- Sparrow/views.py
+++ Sparrow/views.py
@@ -9,6 +9,25 @@ import json, pickle
 from django.http import HttpResponse
 
 from SearchEngine import dosearch,songinfo_loader
+from operator import itemgetter
+def hsort(items, user_history):
+	n = len(items)
+	if not user_history:
+		return items
+	pos = 0
+	for item in items:
+		pos = pos + 1
+		item['sid'] = pos
+		if item['SongID'] in user_history:
+			if pos >= n/2:
+				item['sid'] = pos - (8*pos/n - 3) * user_history[item['SongID']]
+				print((8*pos/n - 3) * user_history[item['SongID']])
+			else:
+				item['sid'] = pos - (2*pos-2)/(n-2) * user_history[item['SongID']]
+				print((2*pos-2)/(n-2) * user_history[item['SongID']])
+	sort_by_pos = sorted(items,key=itemgetter('sid'))
+	return sort_by_pos
+
 
 def search(request):
 	stype = ''
@@ -20,6 +39,14 @@ def search(request):
 	items = {}
 	if stype and stype=='Song/Singer':
 		items = dosearch.dosearch(svalue)
+		# sort according to history
+		if request.user.is_authenticated():
+			user_history = {}
+			if os.path.isfile('static/user_history/{}.json'.format(request.user.id)):
+				f = open('static/user_history/{}.json'.format(request.user.id), 'r', encoding="utf-8")
+				user_history = json.load(f)
+			items = hsort(items, user_history)
+		# end sort
 		paginator = Paginator(items, 25) # Show 25 contacts per page
 		page = request.GET.get('page')
 		try:
@@ -33,8 +60,16 @@ def search(request):
 		return render(request, 'search.html', {'items': items})
 	if stype and stype=='Lyrics':
 		items = dosearch.dosearch_lyric(svalue)
+		# sort according to history
+		if request.user.is_authenticated():
+			user_history = {}
+			if os.path.isfile('static/user_history/{}.json'.format(request.user.id)):
+				f = open('static/user_history/{}.json'.format(request.user.id), 'r', encoding="utf-8")
+				user_history = json.load(f)
+			items = hsort(items, user_history)
 		for item in items:
 			item['Highlights'] = item['Highlights'].replace('\n','<br>')
+		# end sort
 		paginator = Paginator(items, 25) # Show 25 contacts per page
 		page = request.GET.get('page')
 		try:
